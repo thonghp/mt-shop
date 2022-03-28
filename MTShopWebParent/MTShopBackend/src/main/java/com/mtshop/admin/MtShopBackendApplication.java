d08da3b27dba6983e56135ae06efea2e8a76f705
thonghp <thonghoangpham123@gmail.com>
2022-03-28 23:01:26 +0700

    project folder organization

---
+++ MTShopWebParent/MTShopBackend/src/main/java/com/mtshop/admin/MtShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mtshop.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtShopBackendApplication.class, args);
+    }
+
+}
