5fb65a0ce5e64f15fc00f67af57adc93550bea11
thonghp <thonghoangpham123@gmail.com>
2022-03-28 23:52:39 +0700

    add frontend module

---
+++ MTShopWebParent/MTShopFrontend/src/main/java/com/mtshop/MtShopFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.mtshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtShopFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtShopFrontendApplication.class, args);
+    }
+
+}
