09fe4d47157946a498a2c4ebc17a7443a34d25ab
thonghp <thonghoangpham123@gmail.com>
2022-04-02 21:17:01 +0700

    add comments on the use of @repository

--- MTShopWebParent/MTShopBackend/src/main/java/com/mtshop/admin/user/UserRepository.java
+++ MTShopWebParent/MTShopBackend/src/main/java/com/mtshop/admin/user/UserRepository.java
@@ -4,6 +4,13 @@ import com.mtshop.common.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/*
+ * @Repository Mark a Class As a Repository layer (Data Access layer), the class provides the mechanism for storage,
+ * retrieval, search, update and delete operation on objects.
+ * @Repository has been marked with @Component
+ * @Repository converts database exceptions to Spring-based unchecked exceptions
+ */
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 }
