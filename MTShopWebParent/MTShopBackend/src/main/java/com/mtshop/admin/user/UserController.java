2542bee60113d4cdde0e4b30c8f3b1412be051fc
thonghp <thonghoangpham123@gmail.com>
2022-04-05 20:52:45 +0700

    complete the function of adding users

--- MTShopWebParent/MTShopBackend/src/main/java/com/mtshop/admin/user/UserController.java
+++ MTShopWebParent/MTShopBackend/src/main/java/com/mtshop/admin/user/UserController.java
@@ -1,10 +1,13 @@
 package com.mtshop.admin.user;
 
+import com.mtshop.common.entity.Role;
 import com.mtshop.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -12,7 +15,11 @@ import java.util.List;
  * view -> controller -> service -> repository -> entity
  * @Controller is a special case of @Component, use at presentation layer and only apply to class
  * @GetMapping map to http get
- * Model is used to pass data between view and controller, It acts as a data container of the application
+ * Model is an object that stores information and is used by Template Engine to generate webpage. It can be understood
+ * as the Context of Thymeleaf
+ * Model stores information as key-values.
+ * addFlashAttribute() stores the attribute in the flashmap, persists internally in the user session, and is deleted
+ * when a redirect is requested, Multiple object types can be stored inside
  */
 
 @Controller
@@ -27,5 +34,27 @@ public class UserController {
         model.addAttribute("listUsers", listUsers); // = request.setAttribute() of HttpServletRequest
 
         return "users"; // map file users.html
+    }
+
+    @GetMapping("/users/new")
+    public String newUser(Model model) {
+        User user = new User();
+        List<Role> listRoles = userService.listRoles();
+
+        user.setEnabled(true);
+
+        model.addAttribute("user", user);
+        model.addAttribute("listRoles", listRoles);
+
+        return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes redirectAttributes) {
+        userService.save(user);
+
+        redirectAttributes.addFlashAttribute("message","The user has bean saved successfully");
+
+        return "redirect:/users";
     }
 }
--- MTShopWebParent/MTShopBackend/src/main/java/com/mtshop/admin/user/UserService.java
+++ MTShopWebParent/MTShopBackend/src/main/java/com/mtshop/admin/user/UserService.java
@@ -1,5 +1,6 @@
 package com.mtshop.admin.user;
 
+import com.mtshop.common.entity.Role;
 import com.mtshop.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,7 +19,18 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     public List<User> listAll() {
         return (List<User>) userRepository.findAll();
+    }
+
+    public List<Role> listRoles() {
+        return (List<Role>) roleRepository.findAll();
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
     }
 }
