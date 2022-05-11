c5ccf731d6773cea26518518ee7ac0b3da98f336
thonghp <thonghoangpham123@gmail.com>
2022-05-11 23:48:29 +0700

    added function to export data to csv

---
+++ MTShopWebParent/MTShopBackend/src/main/java/com/mtshop/admin/user/UserCSVExporter.java
@@ -0,0 +1,40 @@
+package com.mtshop.admin.user;
+
+import com.mtshop.common.entity.User;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class UserCSVExporter {
+    public void export(List<User> listUsers, HttpServletResponse response) throws IOException {
+        DateFormat dateFormater = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        String timestamp = dateFormater.format(new Date());
+        String fileName = "users_" + timestamp + ".csv";
+
+        response.setContentType("text/csv");
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; fileName=" + fileName;
+        response.setHeader(headerKey, headerValue);
+
+        ICsvBeanWriter csvBeanWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+
+        String[] csvHeader = {"ID", "Email", "Tên", "Họ", "Vai trò", "Trạng thái"};
+        String[] fieldMapping = {"id", "email", "firstName", "lastName", "roles", "enabled"};
+
+        csvBeanWriter.writeHeader(csvHeader);
+
+        for (User user : listUsers) {
+            csvBeanWriter.write(user, fieldMapping);
+        }
+
+        csvBeanWriter.close();
+    }
+}
--- MTShopWebParent/MTShopBackend/src/main/java/com/mtshop/admin/user/UserController.java
+++ MTShopWebParent/MTShopBackend/src/main/java/com/mtshop/admin/user/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
@@ -160,5 +161,12 @@ public class UserController {
         redirectAttributes.addFlashAttribute("message", message);
 
         return "redirect:/users";
+    }
+
+    @GetMapping("/users/export/csv")
+    public void exportToCSV(HttpServletResponse response) throws IOException {
+        List<User> listUsers = userService.listAll();
+        UserCSVExporter exporter = new UserCSVExporter();
+        exporter.export(listUsers, response);
     }
 }
