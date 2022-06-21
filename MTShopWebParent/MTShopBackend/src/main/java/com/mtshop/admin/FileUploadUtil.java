664a524d7db5e369d7533b2aae9495e10eb5f841
18130138@st.hcmuaf.edu.vn <18130138@st.hcmuaf.edu.vn>
2022-06-21 10:25:34 +0700

    add remove dir function

--- MTShopWebParent/MTShopBackend/src/main/java/com/mtshop/admin/FileUploadUtil.java
+++ MTShopWebParent/MTShopBackend/src/main/java/com/mtshop/admin/FileUploadUtil.java
@@ -1,5 +1,7 @@
 package com.mtshop.admin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -10,6 +12,8 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public class FileUploadUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadUtil.class);
+
     public static void saveFile(String uploadDir, String fileName, MultipartFile multipartFile) throws IOException {
         Path uploadPath = Paths.get(uploadDir);
 
@@ -34,12 +38,25 @@ public class FileUploadUtil {
                     try {
                         Files.delete(file);
                     } catch (IOException e) {
-                        System.out.println("Could not delete file: " + file);
+                        LOGGER.error("Could not delete file: " + file);
+//                        System.out.println("Could not delete file: " + file);
                     }
                 }
             });
         } catch (IOException e) {
-            System.out.println("Could not list directory: " + dirPath);
+            LOGGER.error("Could not list directory: " + dirPath);
+//            System.out.println("Could not list directory: " + dirPath);
+        }
+    }
+
+    public static void removeDir(String dir) {
+        cleanDir(dir);
+
+        try {
+            Files.delete(Paths.get(dir));
+        } catch (IOException e) {
+            LOGGER.error("Could not remove directory: " + dir);
+//            System.out.println("Could not remove directory: " + dir);
         }
     }
 }
