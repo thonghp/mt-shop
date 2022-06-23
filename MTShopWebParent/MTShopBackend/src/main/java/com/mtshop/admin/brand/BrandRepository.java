8fdda61f61d1d919f4b3014068f9270737694768
18130138@st.hcmuaf.edu.vn <18130138@st.hcmuaf.edu.vn>
2022-06-23 16:54:32 +0700

    add entity, repository and test

---
+++ MTShopWebParent/MTShopBackend/src/main/java/com/mtshop/admin/brand/BrandRepository.java
@@ -0,0 +1,30 @@
+package com.mtshop.admin.brand;
+
+import com.mtshop.common.entity.Brand;
+import com.mtshop.common.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+
+public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer> {
+//    @Query("UPDATE Category c SET c.enabled = ?2 WHERE c.id = ?1")
+//    @Modifying
+//    void updateEnabledStatus(Integer id, boolean enabled);
+//
+//    List<Category> findByParentIsNull(Sort sort);
+//
+//    Page<Category> findByParentIsNull(Pageable pageable);
+//
+//    Page<Category> findByName(Pageable pageable, String keyword);
+//
+//    Category findByName(String name);
+//
+//    Category findByAlias(String alias);
+//
+//    Long countById(Integer id);
+}
