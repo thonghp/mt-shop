f6a9a196aefefc676b8b56ffdabd22563ca3e967
= <=>
2022-06-24 00:08:20 +0700

    add test function

--- MTShopCommon/src/main/java/com/mtshop/common/entity/Product.java
+++ MTShopCommon/src/main/java/com/mtshop/common/entity/Product.java
@@ -195,4 +195,9 @@ public class Product {
     public void setBrand(Brand brand) {
         this.brand = brand;
     }
+
+    @Override
+    public String toString() {
+        return "Product{" + "id=" + id + ", name='" + name + '\'' + '}';
+    }
 }
